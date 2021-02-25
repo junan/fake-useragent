c74af6d178117be760a261a352c8b33f445aefdd
Junan Chakma <junan@nimblehq.co>
2021-02-25 10:10:29 +0700

    Updated json

--- setting/setting.go
+++ setting/setting.go
@@ -10,7 +10,7 @@ const (
 	BROWSER_ALLOW_MAX_PAGE = 8
 
 	CACHE_VERSION = "0.2.0"
-	CACHE_URL     = "https://raw.githubusercontent.com/EDDYCJY/fake-useragent/v0.2.0/static/"
+	CACHE_URL     = "https://raw.githubusercontent.com/junan/fake-useragent/master/static/"
 
 	HTTP_TIMEOUT         = 5 * time.Second
 	HTTP_DELAY           = 100 * time.Millisecond
